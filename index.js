e15935581eb5b6c215e9365a81da84ce71665ddc
vineetam <vineetam@chetu.com>
2016-07-14 14:31:17 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+var https = require('https');
+
+process.on('uncaughtException', function (err) {
+    console.log(err);
+}); 
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+var makeRequest = function() {
+
+    //var message = "Here's looking at you, kid.";
+    var options = {
+        host: 'localhost', port: 8000, path:'/', method: 'GET'
+    }
+
+    var request = https.request(options, function(response) {
+       
+            console.log('STATUS: ' + response.statusCode);
+            
+            
+            
+        
+    });
+   
+   request.on('response', function (response) {
+  var body = '';
+  response.on('data', function (chunk) {
+    body += chunk;
+  });
+  response.on('end', function () {
+    console.log('BODY: ' + body);
+  });
+});
+request.end();
+};
+
+makeRequest();
+module.exports = makeRequest;
+
+
